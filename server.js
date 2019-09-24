c02693a9184dd5f6964bc36490df4b8ec45bce2c
pechuga22 <iefaz.santiagovalle@gmail.com>
2019-09-24 15:13:12 -0500

    update

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const express = require('express')
+const bodyParser = require('body-parser');
 const cors = require('cors')
 const app = express()
 
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+//connected with db
+require('./src/database_mongo')
 
 //middlewares
 app.use(express.json())
@@ -9,9 +16,10 @@ app.use(cors())
 
 
 //routes
-app.use(require('./routes/router'))
+require('./src/routes/user')(app);
 
-app.set('port', process.env.PORT || 300)
+//Server
+app.set('port', process.env.PORT || 3000)
 
 
 app.listen(app.get('port'), () => console.log(`listening on ${app.get('port')}... `))
