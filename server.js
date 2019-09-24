c9123b13e585c0eb416c3a9cf0032884ea99ed8c
oela <oela@azulBook.local>
2019-09-24 13:01:35 -0500

    get por defecto

--- server.js
+++ server.js
@@ -2,16 +2,17 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 
-
 //middlewares
 app.use(express.json())
 app.use(cors())
 
-
 //routes
-app.use(require('./src/routes/router'))
+// app.use(require('./src/routes/router'))
+app.get('/', (req, res) => {
+    res.send("Ésta es la API de movilapp ✌️")
+})
+
 
 app.set('port', process.env.PORT || 3000)
-
 
 app.listen(app.get('port'), () => console.log(`listening on ${app.get('port')}... `))
