15aec4c80ba14773049df0f07215cffbe29db895
cold-briu <andresavena19@gmail.com>
2019-10-06 13:37:42 -0500

    change res status on login

--- routes/usuarios.route.js
+++ routes/usuarios.route.js
@@ -35,9 +35,9 @@ module.exports = function usuariosApi(app) {
       const user = await userService.login(email);
 
       if (!user) {
-        return res.status(204).json({ authRes: "email not found" });
+        return res.status(400).json({ authRes: "email not found" });
       } else if (user.password !== password) {
-        return res.status(204).json({ authRes: "wrong pass" });
+        return res.status(400).json({ authRes: "wrong pass" });
       }
       res.status(200).send(true);
     } catch (err) {
